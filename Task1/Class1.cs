593d610c30ddb177d997a923863eec3fcaa91c76
Pavel <sozonovps@gmail.com>
2023-01-24 12:49:33 +0800

    Добавьте файлы проекта.

---
+++ Task1/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Angle
+    {
+        int gradus, min, sec;
+
+        public int Gradus
+        { 
+            get 
+            { 
+                return gradus; 
+            }
+            set 
+            {
+                
+                gradus = value % 360;
+            }
+        }
+        public int Min
+        {
+            get 
+            { 
+                return min;
+            }
+            set 
+            {
+                Gradus += Math.Sign(gradus)*value / 60;
+                min = value % 60;
+            }
+        } 
+        public int Sec
+        { 
+            get 
+            {
+                return sec;
+            } 
+            set
+            {
+                Min += Math.Sign(gradus) * value / 60;
+                sec = value%60;
+            }
+        }
+
+       /* public Angle (int gradus, int min, int sec)
+        {
+            this.gradus = gradus;
+            this.min = min;
+            this.sec = sec;
+        }*/
+
+        public void ToRadians()
+        {
+            double rad;
+            rad = (gradus + Math.Sign(gradus) * min /60.0 + Math.Sign(gradus) * sec /3600.0)/180*Math.PI;
+            Console.WriteLine("Угол равен {0} радианам",rad);
+        }
+        double ReturnInRadians()
+        {
+            double rad;
+            rad = (gradus + Math.Sign(gradus) * min / 60.0 + Math.Sign(gradus) * sec / 3600.0) / 180 * Math.PI;
+            return rad;
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle angle = new Angle();
+            Console.WriteLine("Данная программа переведет введенный угол в радианы.");
+            try
+            {
+                Console.WriteLine("Введите градусы");
+                angle.Gradus = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите минуты");
+                angle.Min = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите секунды");
+                angle.Sec = Convert.ToInt32(Console.ReadLine());
+                angle.ToRadians();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message) ;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
